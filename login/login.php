b92933f95b797f2b9ad276db4205d42bac427cb7
lvasil01 <47333447+lvasil01@users.noreply.github.com>
2019-02-26 22:49:44 +0200

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,73 @@
+<?php
+
+session_start();
+ 
+
+
+require_once 'logChecker.php';
+$app = new logChecker();
+ 
+$login_error_message = '';
+ 
+
+if (!empty($_POST['btnLogin'])) {
+ 
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+ 
+    if ($username == "") {
+        $login_error_message = 'Username field is required!';
+    } else if ($password == "") {
+        $login_error_message = 'Password field is required!';
+    } else {
+        $customer_id = $app->Login($conn,$username, $password); 
+        if($customer_id > 0)
+        {
+            $_SESSION['customer_id'] = $customer_id; 
+            header("Location: welcome.php"); 
+        }
+        else
+        {
+            $login_error_message = 'Invalid login details!';
+        }
+    }
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    
+</head>
+<body>
+ <a href="register.php">Registration form</a>
+    
+<div class="container">    
+        <div class="col-md-2"></div>
+        <div class="col-md-5 well">
+            <h4>Login</h4>
+            <?php
+            if ($login_error_message != "") {
+                echo '<div class="alert alert-danger"><strong>Error: </strong> ' . $login_error_message . '</div>';
+            }
+            ?>
+            <form action="login.php" method="post">
+                <div class="form-group">
+                    <label for="">Username/Email</label>
+                    <input type="text" name="username" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="">Password</label>
+                    <input type="password" name="password" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="btnLogin" class="btn btn-primary" value="Login"/>
+                </div>
+            </form>
+        </div>
+   
+</div>
+ 
+</body>
+</html>
