5866c9c15fe6c1f07e1e57d7dab2d08f3e85e4b2
lvasil01 <47333447+lvasil01@users.noreply.github.com>
2019-03-14 17:19:31 +0200

    Add files via upload

--- registration/regChecker.php
+++ registration/regChecker.php
@@ -1,38 +1,39 @@
 <?php
-require_once '../connect.php'; 
+ 
 
+
+ 
 class Checker
 {
  
     
-    public function Register($DBcon,$name, $surname, $telephone , $email, $username, $password , $sex)
+    public function Register($conn,$name, $surname, $telephone , $email, $username, $password , $sex )
     {
         try {
             //$db = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-            $query = $DBcon->prepare("INSERT INTO customers(name, surname , telephone , email, username, password , sex) VALUES (:name, :surname,:telephone,:email,:username,:password ,:sex)");
+            $query = $conn->prepare("INSERT INTO customers(name, surname , telephone , email, username, password , sex) VALUES (:name, :surname,:telephone,:email,:username,:password ,:sex)");
             $query->bindParam("name", $name, PDO::PARAM_STR);
             $query->bindParam("surname", $surname, PDO::PARAM_STR);
-             $query->bindParam("telephone", $telephone, PDO::PARAM_STR);
+            $query->bindParam("telephone", $telephone, PDO::PARAM_STR);
             $query->bindParam("email", $email, PDO::PARAM_STR);
             $query->bindParam("username", $username, PDO::PARAM_STR);
             $enc_password = hash('sha256', $password);
             $query->bindParam("password", $enc_password, PDO::PARAM_STR);
             $query->bindParam("sex", $sex, PDO::PARAM_STR);
-            $query->execute();
-      
              
-            return $DBcon->lastInsertId();
+            $query->execute();
+            return $conn->lastInsertId();
         } catch (PDOException $e) {
             exit($e->getMessage());
         }
     }
  
    
-    public function isUsername($DBcon,$username)
+    public function isUsername($conn,$username)
     {
         try {
             //$db = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-            $query = $DBcon->prepare("SELECT customer_id FROM customers WHERE username=:username");
+            $query = $conn->prepare("SELECT customer_id FROM customers WHERE username=:username");
             $query->bindParam("username", $username, PDO::PARAM_STR);
             $query->execute();
             if ($query->rowCount() > 0) {
@@ -46,11 +47,11 @@ class Checker
     }
  
    
-    public function isEmail($DBcon,$email)
+    public function isEmail($conn,$email)
     {
         try {
           //  $db = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-            $query = $DBcon->prepare("SELECT customer_id FROM customers WHERE email=:email");
+            $query = $conn->prepare("SELECT customer_id FROM customers WHERE email=:email");
             $query->bindParam("email", $email, PDO::PARAM_STR);
             $query->execute();
             if ($query->rowCount() > 0) {
